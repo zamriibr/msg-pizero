ac5c8ca24294d15329e6196e182b27b9436fd408
zamriibr <48670145+zamriibr@users.noreply.github.com>
2025-04-29 22:49:30 +0800

    add count to device

--- function_app.py
+++ function_app.py
@@ -1,5 +1,11 @@
 import azure.functions as func
 import logging
+import os
+from azure.iot.hub import IoTHubRegistryManager
+
+CONNECTION_STRING = os.environ["DEVICE_CONNECTION_STRING"]
+DEVICE_ID = os.environ["DEVICE_ID"]
+
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -7,19 +13,16 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def http_trigger_pi(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    count = req.params.get('count')
+    if not count:
+        return func.HttpResponse("Please pass a 'count' parameter.", status_code=400)
 
-    if name:
-        return func.HttpResponse(f"Yippe, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    try:
+        registry_manager = IoTHubRegistryManager(CONNECTION_STRING)
+
+        message = str(count)
+        registry_manager.send_c2d_message(DEVICE_ID, message)
+
+        return func.HttpResponse(f"Sent blink command: {count}")
+    except Exception as e:
+        return func.HttpResponse(f"Error: {str(e)}", status_code=500)
